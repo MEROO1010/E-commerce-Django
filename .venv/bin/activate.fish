78e19103a8d4ba70ceb81ac0331c9f09f28f3501
Marwah Alsaggaf <72358981+MEROO1010@users.noreply.github.com>
2024-03-22 20:05:02 +0000

    crete folders

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/E-commerce-Django-website-/.venv"
+set -gx VIRTUAL_ENV "/workspaces/E-commerce-Django/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
